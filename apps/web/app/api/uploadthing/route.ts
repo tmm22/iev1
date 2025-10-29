8bd23991783d50190c60ca6912e2d39d73f33d01
Deborah Mangan <your.email@example.com>
2025-10-29 18:06:26 +1000

    K

--- apps/web/app/api/uploadthing/route.ts
+++ apps/web/app/api/uploadthing/route.ts
@@ -1,6 +1,6 @@
 import { createRouteHandler } from "uploadthing/next";
 
-import { editorFileRouter } from "../core";
+import { editorFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: editorFileRouter
